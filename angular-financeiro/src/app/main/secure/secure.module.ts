812288b662d22dcee74cbd70fc639c4f924d3221
Jonatas O <jonatas.oliveira@optimumsupply.com.br>
2022-11-02 20:18:10 -0300

    improvments graphs

--- angular-financeiro/src/app/main/secure/secure.module.ts
+++ angular-financeiro/src/app/main/secure/secure.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { SecureComponent } from './secure.component';
 import { GraphsComponent } from './graphs/graphs.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-
+import * as CanvasJSAngularChart from '../../../assets/canvasjs.angular.component';
+var CanvasJSChart = CanvasJSAngularChart.CanvasJSChart;
+ 
 
 @NgModule({
   declarations: [
     SecureComponent,
-    GraphsComponent
+    GraphsComponent,
+    CanvasJSChart
   ],
   imports: [
     SharedModule
