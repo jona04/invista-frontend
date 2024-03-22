a7009177f98b3031963c36f6c2110e89543b2959
Jonatas Oliveira <jonatas.iw@gmail.com>
2024-03-22 20:54:04 -0300

    new env

--- angular-admin/src/environments/environment.prod.ts
+++ angular-admin/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // api: 'http://localhost:8002/api/admin',
   // api: 'https://admin-api.invistapublicidade.com/api/admin'
-  api: 'https://invista-backend-b24b71ce6ad9.herokuapp.com/'
+  api: 'https://invista-backend-b24b71ce6ad9.herokuapp.com/api/admin'
 };
